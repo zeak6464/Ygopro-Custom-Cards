24ab808ff0b89b4281be72fca121812f1cd070de
zeak6464 <zeak6464@live.com>
2020-10-07 14:08:00 -0400

    Update c6565.lua
    yup

--- script/c6565.lua
+++ script/c6565.lua
@@ -5,7 +5,7 @@ function s.initial_effect(c)
 end
 function s.init(c)
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_ACTIVATE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_PREDRAW)
 	e1:SetCountLimit(1)
 	e1:SetOperation(s.play)
