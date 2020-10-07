5f06e1d97e69810203d24c6f0f4dc7e80ac72911
zeak6464 <zeak6464@live.com>
2020-10-07 14:11:02 -0400

    Update 65651.lua
    more testing

--- script/65651.lua
+++ script/65651.lua
@@ -1,5 +1,12 @@
+Debug.ReloadFieldBegin()
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
 --Hand (yours)
 Debug.AddCard(24224830,0,0,LOCATION_HAND,0,POS_FACEDOWN)
 
 --Monster Zones (yours)
 Debug.AddCard(37818794,0,0,LOCATION_MZONE,2,POS_FACEUP_ATTACK,true)
+
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
