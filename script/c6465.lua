bc5f0f3e2847035523c520155d8c730eb4788edb
zeak6464 <zeak6464@live.com>
2020-11-18 15:22:53 -0500

    Update c6465.lua
    more

--- script/c6465.lua
+++ script/c6465.lua
@@ -13224,7 +13224,7 @@ if not SealedDuel then
 				local fg=Duel.GetFieldGroup(p,0xff,0)
 				local exclude=fg:Select(p,0,#fg-20,nil) 
 				
-				if #fg>60 then
+				if #fg<60 then
 				   Duel.SendtoDeck(exclude,nil,-2,REASON_RULE)
 				end
 				
