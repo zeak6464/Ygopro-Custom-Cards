b57122374d624733104338006f5ea90ccbec6dc6
zeak6464 <zeak6464@live.com>
2020-11-18 19:00:57 -0500

    Update c6465.lua

--- script/c6465.lua
+++ script/c6465.lua
@@ -13258,7 +13258,7 @@ if not SealedDuel then
 		end
 		
 				-- New Code to Edit Deck 
-		if Duel.SelectYesNo(p,aux.Stringid(6465,8)) and #dg1<39 then
+		if Duel.SelectYesNo(p,aux.Stringid(6465,8)) then
 		    local dg1=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_DECK,0,nil)
 			local fg=Duel.GetFieldGroup(p,0xff,0)
 			local exclude=fg:Select(p,0,#fg-20,nil)
