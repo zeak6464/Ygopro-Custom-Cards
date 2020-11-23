a6bc336c191b9c504d196e0afa1a49ab5bafe111
zeak6464 <zeak6464@live.com>
2020-11-23 00:20:44 -0500

    Update c6464.lua

--- script/c6464.lua
+++ script/c6464.lua
@@ -31,7 +31,7 @@ function s.play(e,tp,eg,ep,ev,re,r,rp)
 	    Duel.Draw(1-tp,h2,REASON_EFFECT)
 	elseif dice==3 then
 		Debug.ShowHint("Remove all cards from both players graveyards from play.")
-	 local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE,nil)
+	 local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE,LOCATION_GRAVE,nil)
 	    Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
 	elseif dice==4 then
 	    Debug.ShowHint("Destory all monsters on the field.")
