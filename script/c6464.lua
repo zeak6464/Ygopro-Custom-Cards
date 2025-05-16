df6e3df72ac2ff33724c45ced3377ce5bf0722e2
zeak6464 <zeak6464@live.com>
2025-05-15 20:40:41 -0400

    Update c6464.lua

--- script/c6464.lua
+++ script/c6464.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
 	
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
 	
