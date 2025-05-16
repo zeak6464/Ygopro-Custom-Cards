454fc21bb04d2ad4d81bb94a9c66964968b9586d
zeak6464 <zeak6464@live.com>
2025-05-15 21:52:28 -0400

    Update c6464.lua

--- script/c6464.lua
+++ script/c6464.lua
@@ -6,14 +6,19 @@ local s,id=GetID()
 --function s.init(c)
 function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_PREDRAW)
 	e1:SetCountLimit(1)
+	e1:SetCondition(s.con)
 	Debug.ShowHint("Greetings Duelists!")
 	e1:SetOperation(s.operation)
-	Duel.RegisterEffect(e1,0)
+	Duel.RegisterEffect(e1)
 end
 
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnCount()==1
+end
 
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,tp,id)
