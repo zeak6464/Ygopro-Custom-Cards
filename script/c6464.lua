1c2725baa9bbae46b1696e69579251f45886ed25
zeak6464 <zeak6464@live.com>
2025-05-15 20:43:05 -0400

    Update c6464.lua

--- script/c6464.lua
+++ script/c6464.lua
@@ -10,9 +10,8 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PREDRAW)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
-	e1:SetOperation(s.play)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
 	Duel.RegisterEffect(e1,0)
 	
 	--Add counter
